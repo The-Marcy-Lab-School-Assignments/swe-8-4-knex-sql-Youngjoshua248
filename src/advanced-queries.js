1894b50b5de73c76a7f2567944de30c8d74cf451
Joshua Young <youngjoshua248@icloud.com>
2025-08-02 22:04:14 -0400

    Fully finished

--- src/advanced-queries.js
+++ src/advanced-queries.js
@@ -1,59 +1,79 @@
-const knex = require('./knex');
+const knex = require("./knex");
 
 const countNumberOfBooks = async () => {
-  const query = ``;
+  const query = `
+    SELECT COUNT(*) AS count
+    FROM books;
+  `;
 
-  // const { rows } = await knex.raw(query);
-  // console.log('Number of books:', rows);
-  // return rows;
+  const { rows } = await knex.raw(query);
+  return rows[0];
 };
 
 const selectAllLongOrMovieBooks = async () => {
-  const query = ``;
+  const query = `
+    SELECT *
+    FROM books
+    WHERE pages >= 250 OR is_movie = true;
+  `;
 
-  // const { rows } = await knex.raw(query);
-  // console.log('Long or movie books:', rows);
-  // return rows;
+  const { rows } = await knex.raw(query);
+  return rows;
 };
 
 const selectBooksBetween150And300Pages = async () => {
-  const query = ``;
+  const query = `
+    SELECT *
+    FROM books
+    WHERE pages > 150 AND pages < 300;
+  `;
 
-  // const { rows } = await knex.raw(query);
-  // console.log('150-300:', rows);
-  // return rows;
+  const { rows } = await knex.raw(query);
+  return rows;
 };
 
 const orderBooksByPages = async () => {
-  const query = ``;
+  const query = `
+    SELECT *
+    FROM books
+    ORDER BY pages ASC;
+  `;
 
-  // const { rows } = await knex.raw(query);
-  // console.log('Short to long:', rows);
-  // return rows;
+  const { rows } = await knex.raw(query);
+  return rows;
 };
 
 const selectLongestBook = async () => {
-  const query = ``;
+  const query = `
+    SELECT *
+    FROM books
+    ORDER BY pages DESC
+    LIMIT 1;
+  `;
 
-  // const { rows } = await knex.raw(query);
-  // console.log('Longest Book:', rows);
-  // return rows;
+  const { rows } = await knex.raw(query);
+  return rows;
 };
 
 const aliasIsMovie = async () => {
-  const query = ``;
+  const query = `
+    SELECT title, is_movie AS "Already Filmed"
+    FROM books;
+  `;
 
-  // const { rows } = await knex.raw(query);
-  // console.log('Fancy output', rows);
-  // return rows;
+  const { rows } = await knex.raw(query);
+  return rows;
 };
 
 const countBooksInGenres = async () => {
-  const query = ``;
+  const query = `
+    SELECT genre, COUNT(*) AS count
+    FROM books
+    GROUP BY genre;
+  `;
 
-  // const { rows } = await knex.raw(query);
-  // console.log('Genre count', rows);
-  // return rows;
+  const { rows } = await knex.raw(query);
+  return rows;
 };
 
 module.exports = {
